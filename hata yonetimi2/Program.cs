0ca204a17792f7bd3cd443b591cb827019d2ffb6
omerfarukkaya590 <85821993+omerfarukkaya590@users.noreply.github.com>
2022-01-14 20:03:20 +0300

    Add files via upload

---
+++ hata yonetimi2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace hata_yonetimi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{Console.WriteLine("Bir sayı giriniz:");
+            //int sayı =Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Girmiş oldupunuz sayı şudur:" + sayı);
+            //}
+            //catch(Exception ex)
+            //{
+            //Console.WriteLine("Hata: "+ ex.Message.ToString());
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("İşlem tamamlandı.");
+            //}
+            try
+            {
+                 //int a=int.Parse("test");
+                 //int a =int.Parse(null);
+                 int a= int.Parse("-20000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdniz.");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)  
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük ya da çok büyük değer girdniz.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem başarıyla tamamlandı.");
+            }
+        }
+    }
+}
